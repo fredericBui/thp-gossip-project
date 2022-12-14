41d239e345d217037eb3d9f4c9c37d2bff44e8b7
Frédéric Bui <bui.frederic.pro@gmail.com>
2022-12-14 10:38:41 +0100

    add ref user to city

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_14_092947) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_14_093623) do
   create_table "cities", force: :cascade do |t|
     t.string "name"
     t.string "zip_code"
@@ -23,6 +23,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_14_092947) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_gossips_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,5 +39,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_14_092947) do
     t.index ["city_id"], name: "index_users_on_city_id"
   end
 
+  add_foreign_key "gossips", "users"
   add_foreign_key "users", "cities"
 end
