22ceea323262c55d8bb09af063eddea2f046da00
Frédéric Bui <bui.frederic.pro@gmail.com>
2022-12-14 11:07:31 +0100

    migrate model tag

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_14_093623) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_14_095531) do
   create_table "cities", force: :cascade do |t|
     t.string "name"
     t.string "zip_code"
@@ -25,6 +25,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_14_093623) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.index ["user_id"], name: "index_gossips_on_user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
